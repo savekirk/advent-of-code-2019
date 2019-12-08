239638c69408964805ce598a8c7ce64449cc9992
Kirk Agbenyegah <savekirk@gmail.com>
2019-12-08 18:06:54 +0300

    Day 1 - Part 2

--- day-1/solution.nim
+++ day-1/solution.nim
@@ -3,9 +3,20 @@ import strutils, sequtils, math
 proc fuelRequired(mass: int): int =
   mass.div(3) - 2
 
+proc additionalFuelRequired(mass: int): int =
+  var newMass = fuelRequired(mass)
+  var addFuel = 0
+  while newMass > 0:
+    addFuel.inc(newMass)
+    newMass = fuelRequired(newMass)
+
+  addFuel
+
 let
   strMasses = readFile("input.txt").strip().splitLines()
-  individualFuel = strMasses.map(parseInt).map(fuelRequired)
-  totalFuelRequired = individualFuel.sum()
+  masses = strMasses.map(parseInt)
+  totalFuelRequired = masses.map(fuelRequired).sum()
+  totalAdditionalFuelRequired = masses.map(additionalFuelRequired).sum()
 
 echo totalFuelRequired
+echo totalAdditionalFuelRequired
